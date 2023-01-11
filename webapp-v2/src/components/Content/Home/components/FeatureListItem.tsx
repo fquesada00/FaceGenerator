8bad516d73e272c79bb9dc782e009622eeb8a8a4
octavio serpe <octavio.serpe@gmail.com>
2023-01-11 09:17:52 -0300

    :lipstick: improve home links

--- webapp-v2/src/components/Content/Home/components/FeatureListItem.tsx
+++ webapp-v2/src/components/Content/Home/components/FeatureListItem.tsx
@@ -11,9 +11,9 @@ const FeatureListItem = (props: FeatureListItemProps) => {
   const { title, description, path } = props;
 
   return (
-    <li>
+    <li className="list-disc">
       <Typography variant="subtitle1" sx={{ fontWeight: 'medium' }}>
-        <Link to={path} ><strong>{ title }</strong></Link>: { description }
+        <Link to={path} className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"><strong>{ title }</strong></Link>: { description }
       </Typography>
     </li>
   );
--- webapp-v2/src/components/Content/Home/index.tsx
+++ webapp-v2/src/components/Content/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import paths from "routes/paths";
 import faces from "../../../../assets/faces.jpg";
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
           <br /><br />
           Here is a list of the features that are currently available:
         </Typography>
-        <ul>
+        <ul className="pl-4">
           <FeatureListItem title="Random faces" description="Generate an amount of random faces." path={paths.randomFaces} />
           <FeatureListItem title="Search faces" description="Search for faces using an id. Also displays all generated images." path={paths.searchFaces} />
           <FeatureListItem title="Transition faces" description="Generate an amount of faces (transitions) between two faces." path={paths.transitionFaces} />
@@ -25,12 +25,37 @@ const Home: React.FC = () => {
           <FeatureListItem title="Modify face features" description="Modify the features of a generated face." path={paths.faceFeaturesModification} />
           <FeatureListItem title="Interchange features" description="Interchange the features of two faces." path={paths.interchangeFacesFeatures} />
         </ul>
-        <Typography variant="subtitle1" sx={{ fontWeight: 'medium' }}>
-          You can find more information about the project in the <Link to={paths.about} ><strong>About</strong></Link> page.
-        </Typography>
+        <div className="mt-4">
+          <Typography variant="subtitle1" sx={{ fontWeight: 'medium' }}>
+            You can find more information about the project in the
+            <Link to={paths.about} className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">
+              <strong> About </strong>
+            </Link>
+            page.
+          </Typography>
+        </div>
       </Grid>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container direction="column" alignItems="center" justifyContent="center">
-        <img src={faces} alt="faces" style={{height: "25rem", width: "25rem"}}/>
+        <Box
+          sx={{
+            width: {
+              xs: "16rem",
+              sm: "20rem",
+              md: "24rem",
+              lg: "26rem",
+              xl: "30rem",
+            },
+            height: {
+              xs: "16rem",
+              sm: "20rem",
+              md: "24rem",
+              lg: "26rem",
+              xl: "30rem",
+            }
+          }}
+        >
+          <img src={faces} alt="Faces" style={{ height: "inherit", width: "inherit" }} />
+        </Box>
       </Grid>
     </Grid>
   );
