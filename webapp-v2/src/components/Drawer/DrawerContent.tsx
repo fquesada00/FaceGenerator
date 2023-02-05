f9738efd23e7ddf23feb277572d669becf59b9d1
Francisco Quesada <francisco.quesada@mercadolibre.com>
2023-02-05 16:00:23 -0300

    remove unused imports

--- webapp-v2/src/components/Drawer/DrawerContent.tsx
+++ webapp-v2/src/components/Drawer/DrawerContent.tsx
@@ -1,13 +1,4 @@
 import React from "react"
-import HomeIcon from "@mui/icons-material/Home"
-import ImportExportIcon from "@mui/icons-material/ImportExport"
-import SearchIcon from "@mui/icons-material/Search"
-import LayersIcon from "@mui/icons-material/Layers"
-import ImageSearchIcon from "@mui/icons-material/ImageSearch"
-import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural"
-import ShuffleIcon from "@mui/icons-material/Shuffle"
-import InfoIcon from "@mui/icons-material/Info"
-import paths from "routes/paths"
 import DrawerButton from "./DrawerButton"
 import drawerButtons from "./DrawerButtons"
 const DrawerContent = () => {
