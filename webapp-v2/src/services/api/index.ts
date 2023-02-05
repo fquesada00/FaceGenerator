4345d2f9c4a4f467d69c957d77d1ecf0c1383dc8
octavio serpe <octavio.serpe@gmail.com>
2023-02-04 23:21:45 -0300

    :sparkles: improve generic api methods

--- webapp-v2/src/services/api/index.ts
+++ webapp-v2/src/services/api/index.ts
@@ -3,42 +3,48 @@ import client from './Client';
 export const API_PREFIX = '/api';
 export const FACES_API_PREFIX = `${API_PREFIX}/faces`;
 
+type ApiMethodParams = {
+  query?: {[key: string]: any};
+  body?: any;
+  headers?: any;
+};
+
 const api = {
-  get: async <T>(url: string, params: {[key: string]: any} = {}) => {
+  get: async <T>(url: string, { query, headers }: ApiMethodParams = {}) => {
     try {
-      const { data } = await client.get(url, { params });
+      const { data } = await client.get(url, { params: query, headers });
       return data as T;
     } catch (error) {
       throw error;
     }
   },
-  post: async <T>(url: string, params: {[key: string]: any} = {}, body: any = {}, headers: any = {}) => {
+  post: async <T>(url: string, { query, body, headers }: ApiMethodParams = {}) => {
     try {
-      const { data } = await client.post(url, body, { params, headers });
+      const { data } = await client.post(url, body, { params: query, headers });
       return data as T;
     } catch (error) {
       throw error;
     }
   },
-  put: async <T>(url: string, params: {[key: string]: any} = {}, body: any) => {
+  put: async <T>(url: string, { query, body, headers }: ApiMethodParams = {}) => {
     try {
-      const { data } = await client.put(url, body, { params });
+      const { data } = await client.put(url, body, { params: query, headers });
       return data as T;
     } catch (error) {
       throw error;
     }
   },
-  patch: async <T>(url: string, params: {[key: string]: any} = {}, body: any) => {
+  patch: async <T>(url: string, { query, body, headers }: ApiMethodParams = {}) => {
     try {
-      const { data } = await client.patch(url, body, { params });
+      const { data } = await client.patch(url, body, { params: query, headers });
       return data as T;
     } catch (error) {
       throw error;
     }
   },
-  delete: async <T>(url: string, params: {[key: string]: any} = {}) => {
+  delete: async <T>(url: string, { query, headers }: ApiMethodParams = {}) => {
     try {
-      const { data } = await client.delete(url, { params });
+      const { data } = await client.delete(url, { params: query, headers });
       return data as T;
     } catch (error) {
       throw error;
