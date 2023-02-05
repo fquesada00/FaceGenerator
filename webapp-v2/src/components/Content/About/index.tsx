64acb2beb2f4a7e63ffba50e9b3985d008ba1de5
Francisco Quesada <francisco.quesada@mercadolibre.com>
2023-02-05 15:53:25 -0300

    fix buttons and hardcoded text

--- webapp-v2/src/components/Content/About/index.tsx
+++ webapp-v2/src/components/Content/About/index.tsx
@@ -2,7 +2,7 @@ import ContentHeader from "components/ContentHeader";
 import { useMemo } from "react";
 import paths from "routes/paths";
 import AboutSection from "./components/AboutSection";
-
+import aboutJson from "assets/about.json";
 const About: React.FC = () => {
   const renderSubtitle = useMemo(() => {
     return (
@@ -12,6 +12,16 @@ const About: React.FC = () => {
     )
   }, []);
 
+  const aboutAuthors = useMemo(() => ({
+    title: aboutJson['about_authors'].title,
+    content: aboutJson['about_authors'].content
+  }), []);
+
+  const aboutPurpose = useMemo(() => ({
+    title: aboutJson['about_project'].title,
+    content: aboutJson['about_project'].content
+  }), []);
+
   return (
     <div>
       <ContentHeader
@@ -20,14 +30,12 @@ const About: React.FC = () => {
       />
       <div>
         <AboutSection 
-        title="Who is the author of this project?"
-        content="This is a final project for Software Engineering in ITBA. It was started by Jimena Lozano and Maite Herrán, and now it is continued by Nicolás Britos and Agustín Roca. From the beginning this project was tutored by Rodrigo Ramele."
+        title={aboutAuthors.title}
+        content={aboutAuthors.content}
         />
         <AboutSection
-        title="What is the purpose of this project?"
-        content="El Laboratorio de Sueño y Memoria from Instituto Tecnológico de Buenos Aires (ITBA) studies the formation of false memories, and how these can be reduced or modified, and is in collaboration with the Innocence Project to investigate how these can lead to errors in convictions. From this research, the need arises to carry out experiments with human faces that are similar to each other, and how this similarity can result in the formation of false memories.
-
-        In this project, we investigate a field of Artificial Intelligence (AI), Deep Learning, which can provide us with a solution to the generation of artificial faces. In particular, we implement a face generation model using a Generative Adversarial Network (GAN), with the aim of generating faces as realistic as possible, so that a human cannot distinguish them from real faces. StyleGAN, a particular implementation of the GAN network, was the chosen architecture, because in addition to producing images with high resolution quality, it presents a model that allows navigation of the latent space and the synthesis of faces, using style mixing properties."
+        title={aboutPurpose.title}
+        content={aboutPurpose.content}
         />
         </div>
     </div>
---
+++ webapp-v2/src/components/Drawer/DrawerButtons.ts
@@ -0,0 +1,55 @@
+import HomeIcon from "@mui/icons-material/Home"
+import ImportExportIcon from '@mui/icons-material/ImportExport';
+import SearchIcon from '@mui/icons-material/Search';
+import LayersIcon from '@mui/icons-material/Layers';
+import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import InfoIcon from '@mui/icons-material/Info';
+import paths from "routes/paths";
+
+const buttons = [
+  {
+    icon: HomeIcon,
+    text: "Home",
+    path: paths.home.path,
+  },
+  {
+    icon: ShuffleIcon,
+    text: "Random faces",
+    path: `${paths.home.path}/${paths.randomFaces.path}`,
+  },
+  {
+    icon: SearchIcon,
+    text: "Search faces",
+    path: `${paths.home.path}/${paths.searchFaces.path}`,
+  },
+  {
+    icon: LayersIcon,
+    text: "Transition faces",
+    path: `${paths.home.path}/${paths.transitionFaces.path}`,
+  },
+  {
+    icon: ImageSearchIcon,
+    text: "Face from image",
+    path: `${paths.home.path}/${paths.faceFromImage.path}`,
+  },
+  {
+    icon: FaceRetouchingNaturalIcon,
+    text: "Modify face features",
+    path: `${paths.home.path}/${paths.faceFeaturesModification.path}`,
+  },
+  {
+    icon: ImportExportIcon,
+    text: "Interchange features",
+    style: { transform: "rotate(90deg)" },
+    path: `${paths.home.path}/${paths.interchangeFacesFeatures.path}`,
+  },
+  {
+    icon: InfoIcon,
+    text: "About",
+    path: paths.about.path,
+  }
+]
+
+export default buttons
