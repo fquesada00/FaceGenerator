d21da7cac14bbe056a34bfdc2bcd8c964eda7452
Francisco Quesada <fquesada@itba.edu.ar>
2023-02-06 09:19:24 -0300

    refactor about

--- webapp-v2/src/components/Content/About/index.tsx
+++ webapp-v2/src/components/Content/About/index.tsx
@@ -1,45 +1,47 @@
-import ContentHeader from "components/ContentHeader";
-import { useMemo } from "react";
-import paths from "routes/paths";
-import AboutSection from "./components/AboutSection";
-import aboutJson from "assets/about.json";
+import ContentHeader from "components/ContentHeader"
+import { useCallback, useMemo } from "react"
+import paths from "routes/paths"
+import AboutSection from "./components/AboutSection"
+import aboutJson from "assets/data/about.json"
 const About: React.FC = () => {
   const renderSubtitle = useMemo(() => {
     return (
       <div>
-        Here you can find information about the project, its purpose and the technologies used. Along with . . .
+        Here you can find information about the project, its purpose and the
+        technologies used. Along with . . .
       </div>
     )
-  }, []);
+  }, [])
 
-  const aboutAuthors = useMemo(() => ({
-    title: aboutJson['about_authors'].title,
-    content: aboutJson['about_authors'].content
-  }), []);
+  const AboutAuthors: React.FC = useCallback(
+    () => (
+      <AboutSection
+        title={aboutJson["about_authors"].title}
+        content={aboutJson["about_authors"].content}
+      />
+    ),
+    []
+  )
 
-  const aboutPurpose = useMemo(() => ({
-    title: aboutJson['about_project'].title,
-    content: aboutJson['about_project'].content
-  }), []);
+  const AboutProject: React.FC = useCallback(
+    () => (
+      <AboutSection
+        title={aboutJson["about_project"].title}
+        content={aboutJson["about_project"].content}
+      />
+    ),
+    []
+  )
 
   return (
     <div>
-      <ContentHeader
-        title={paths.about.title}
-        subtitle={renderSubtitle}
-      />
+      <ContentHeader title={paths.about.title} subtitle={renderSubtitle} />
       <div>
-        <AboutSection 
-        title={aboutAuthors.title}
-        content={aboutAuthors.content}
-        />
-        <AboutSection
-        title={aboutPurpose.title}
-        content={aboutPurpose.content}
-        />
-        </div>
+        <AboutAuthors />
+        <AboutProject />
+      </div>
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
