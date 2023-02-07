b9f251f35fb366d9c4a4a8f0e5310980301716dc
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2023-02-07 23:24:07 +0000

    remove unnecesary code in database.py

--- api/src/service/database.py
+++ api/src/service/database.py
@@ -1,18 +1,6 @@
-import sqlite3
 import numpy as np
 import io
 from pg import DB
-
-def adapt_array(arr):
-    out = io.BytesIO()
-    np.save(out, arr)
-    out.seek(0)
-    return sqlite3.Binary(out.read())
-
-def convert_array(text):
-    out = io.BytesIO(text)
-    out.seek(0)
-    return np.load(out, allow_pickle=True)
 
 class GeneratorDB:
     def __init__(self):
