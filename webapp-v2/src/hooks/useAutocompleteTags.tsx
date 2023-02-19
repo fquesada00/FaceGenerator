32265c070cb481e2f1e0ce42b4626e32bde752cf
octavioserpe <52381091+OctavioSerpe@users.noreply.github.com>
2023-02-19 19:53:40 -0300

    Fix/frontend multiple tags api calls (#21)
    * :lipstick: improve modify features filters display
    
    * :alien: update proxy host
    
    * :bug: fix getAllTags being called multiple times
    
    * :recycle: refactor use tags with formik

--- webapp-v2/src/hooks/useAutocompleteTags.tsx
+++ webapp-v2/src/hooks/useAutocompleteTags.tsx
@@ -1,37 +1,48 @@
-import useAutocompleteChipsInput from 'components/Inputs/useAutocompleteChipsInput';
+import { FormikAutoCompleteTagsProps } from "components/Inputs/formik/models/FormikAutoCompleteTagsModels";
+import useAutocompleteChipsInput from "components/Inputs/useAutocompleteChipsInput";
+import { useEffect } from "react";
+import { useMutation } from "react-query";
 import { toastError } from 'components/Toast';
-import { useQuery } from 'react-query';
 import ApiError from 'services/api/Error';
-import useFacesApi from './api/useFacesApi';
+import useFacesApi from "./api/useFacesApi";
+
 
 type AutocompleteTags = {
   label?: string;
   allowUserInput?: boolean;
+  formikAutoCompleteTagsProps: FormikAutoCompleteTagsProps;
 };
 
 const useAutocompleteTags = (props: AutocompleteTags) => {
+  const { label = 'Tags', allowUserInput, formikAutoCompleteTagsProps } = props;
   const { getAllTags } = useFacesApi();
-  const { label = 'Tags', allowUserInput } = props;
 
-  const { isLoading: isLoadingTags, data: tags } = useQuery(
-    'tags',
-    getAllTags,
-    {
-      onError: error => {
-        if (error instanceof ApiError) {
-          toastError(error.toString());
-        }
+  const {
+    mutate: mutateGetAllTags,
+    isLoading: isLoadingTags,
+    data: tags
+  } = useMutation(getAllTags, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      if (error instanceof ApiError) {
+        toastError(error.toString());
       }
     }
-  );
+  });
 
-  const { content: Autocomplete, value: selectedTags } =
-    useAutocompleteChipsInput({
-      options: tags,
-      value: [],
-      label,
-      allowUserInput
-    });
+  const { content: Autocomplete, value: selectedTags } = useAutocompleteChipsInput({
+    options: tags,
+    value: [],
+    label: label,
+    allowUserInput: allowUserInput,
+    formikAutoCompleteTagsProps
+  });
+
+  useEffect(() => {
+    mutateGetAllTags();
+  }, []);
 
   return {
     Autocomplete,
