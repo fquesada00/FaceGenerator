1beb9429e68addabad7f3f58f3434ac1dfd49281
Agustin Jerusalinsky <ajerusalinsky@itba.edu.ar>
2023-03-26 00:58:04 -0300

    stylegan3 migration

--- generator/server.py
+++ generator/server.py
@@ -23,7 +23,7 @@ ns_thread.start()
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
-generator = Generator("gdrive:networks/stylegan2-ffhq-config-f.pkl")
+generator = Generator("http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-f.pkl")
 uri = daemon.register(generator)   # register the greeting maker as a Pyro object
 ns.register("facegenerator.generator", uri)   # register the object with a name in the name server
 
