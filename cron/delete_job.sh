fe9087952c5813fbe5316732cb1bcb9e438f5839
Octavio Javier Serpe <oserpe@itba.edu.ar>
2023-04-18 13:38:05 +0000

    :art: improve cron job and add README

--- cron/delete_job.sh
+++ cron/delete_job.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# validate if the script is being run as root or with sudo privileges
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo privileges" >&2
+    exit 1
+fi
+
+# Set the regex pattern to match a specific cronjob
 regex=$(echo "0 0 * * * sudo -u root rm -f" | sed 's/[[\.*^$/]/\\&/g')
 
-crontab -l | sed "/^\s*$regex\b/d" | crontab -
+# Validate if the regex pattern is set
+if [ -z "$regex" ]; then
+  printf "Error: Regex pattern is not set\n" >&2
+  exit 1
+fi
+
+# Check if the cron exists
+crontab=$(crontab -l 2>/dev/null)
+if ! echo "$crontab" | grep -q "$regex"; then
+  printf "Cron does not exist\n"
+  exit 0
+fi
+
+# Remove the matching cronjob from the crontab
+new_crontab=$(echo "$crontab" | sed "/^\s*$regex\b/d")
+echo "$new_crontab" | crontab - || {
+  printf "Error: Failed to update crontab\n" >&2
+  exit 1
+}
+
+printf "Cron job removed successfully\n"
+
+# Exit with a success status code
+exit 0
--- cron/remove_tmp_files_job.sh
+++ cron/remove_tmp_files_job.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
-(crontab -l ; echo "0 0 * * * sudo -u root rm -f $PWD/../api/faces/tmp/*") | crontab -
+# this script installs a cron job to run a command that deletes files from a specific directory
+# at midnight every day
 
-echo "Cron job installed successfully"
+# validate if the script is being run as root or with sudo privileges
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo privileges" >&2
+    exit 1
+fi
+
+# validate if the directory to be cleaned exists and is writable
+dir="$PWD/../api/faces/tmp"
+if [ ! -d "$dir" ]; then
+    echo "Directory $dir does not exist" >&2
+    exit 1
+elif [ ! -w "$dir" ]; then
+    echo "Directory $dir is not writable" >&2
+    exit 1
+fi
+
+# install the cron job to run the command that deletes files from the directory at midnight every day
+(crontab -l ; echo "0 0 * * * sudo -u root rm -f $dir/*") | crontab -
+
+# check if the cron job was installed successfully
+if [ $? -eq 0 ]; then
+    echo "Cron job installed successfully"
+else
+    echo "Failed to install cron job" >&2
+    exit 1
+fi
+
+# exit with a success status code
+exit 0
